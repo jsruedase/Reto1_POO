01cf416a9364362262cb370cf633a95688da6d7b
Juan Sebastián Rueda Segura <159072764+jsruedase@users.noreply.github.com>
2024-02-27 20:41:16 -0500

    Update anagrama.py

--- anagrama.py
+++ anagrama.py
@@ -1,3 +1,15 @@
+"""
+Explicación: La función recibe por parámetro una lista de strings, los cuales son analizados en dos ciclos for: el primero coge la palabra, coge las letras de la palabra en
+una lista y crea una lista lista_mismas_letras, donde se meterán los anagramas referentes a esa palabra. El segundo ciclo vuelve a iterar la lista original pero compara las 
+letras de palabra_2 y si encuentra una letra que no se encuentre en la palbra del primer ciclo, no se añade a la lista de anagramas de esta palabra. En caso de que todas sus 
+letras estén, se evalua si la longitud de la palabra sea la misma, sino no son anagramas. Finalmente, por cada palabra se añade a una lista total, la cual no tiene elementos 
+repetidos. Como no se especifica en el enunciado, se toma como consideración que se están buscando los anagramas a la palabra en la primera posición de la lista y esa es la 
+que se imprime.
+
+"""
+
+
+
 def find_anagrams(lista):
     lista_tot = []
     for palabra in lista:
